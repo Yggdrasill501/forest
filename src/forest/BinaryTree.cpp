73ed9ae676225fb84c77b92f12ee73794d099b9f
Filip Žitný <filipzitny@gmail.com>
2024-03-17 19:54:40 +0100

    impl

---
+++ src/forest/BinaryTree.cpp
@@ -0,0 +1,25 @@
+//
+// Created by Filip Žitný on 17/03/2024.
+//
+#include "BinaryTree.h"
+
+BinaryTree::BinaryTree(){
+    root = nullptr;
+}
+
+bool BinaryTree::empty(){
+    return root == nullptr;
+}
+
+void BinaryTree::clear(){
+    popSubTree(root);
+}
+
+void BinaryTree::popSubTree(uNode rootSubTree){
+    if (rootSubTree){
+        popSubTree(rootSubTree -> getRight());
+        popSubTree(rootSubTree -> getLeft());
+        delete rootSubTree;
+}
+
+
